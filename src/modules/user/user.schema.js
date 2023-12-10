c137e38ab7e7bda5d75bc43229ac3eba1415ac95
myunus-info <myunus_info@yahoo.com>
2023-12-10 13:23:13 +0600

    registration schema and login schema modified

--- src/modules/user/user.schema.js
+++ src/modules/user/user.schema.js
@@ -4,6 +4,10 @@ const isUsernameValid = username => {
   return /^[a-z0-9_]+$/.test(username);
 };
 
+const isEmailValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
 const registerSchema = object().shape({
   username: string()
     .max(100, 'This field must be at most 100 characters long')
@@ -11,7 +15,8 @@ const registerSchema = object().shape({
     .test('No special character, space or uppercase in username', username => isUsernameValid(username)),
   email: string()
     .max(100, 'This field must be at most 100 characters long')
-    .required('This field must not be empty'),
+    .required('This field must not be empty')
+    .test('Invalid email address', email => isEmailValid(email)),
   password: string()
     .min(8, 'Password must be at least 8 characters long')
     .max(50, 'This field must be at most 50 characters long')
@@ -22,9 +27,12 @@ const registerSchema = object().shape({
 });
 
 const loginSchema = object().shape({
+  username: string()
+    .max(100, 'This field must be at most 100 characters long')
+    .test('No special character, space or uppercase in username', username => isUsernameValid(username)),
   email: string()
     .max(100, 'This field must be at most 100 characters long')
-    .required('This field must not be empty'),
+    .test('Invalid email address', email => isEmailValid(email)),
   password: string()
     .min(8, 'Password must be at least 8 characters long')
     .max(50, 'This field must be at most 50 characters long')
